6d2da6a9e208f4104e0f7604317ae8d2e7ab1c2e
katsisaac50 <33658908+katsisaac50@users.noreply.github.com>
2024-12-28 13:38:39 +0100

    clean up

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const { registerUser, loginUser } = require('../controllers/authController');
-const { protect } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 // Register Route
@@ -8,11 +7,6 @@ router.post('/register', registerUser);
 
 // Login Route
 router.post('/login', loginUser);
-
-
-router.get('/protected-route', protect, (req, res) => {
-    res.send('This is a protected route');
-});
 
 
 module.exports = router;
