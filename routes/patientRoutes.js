b6194cde192370194ec1a68631d62f06f6c69919
katsisaac50 <33658908+katsisaac50@users.noreply.github.com>
2024-12-28 13:37:17 +0100

    protect get/add patient routes

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const { getPatients, addPatient, getPatientById } = require('../controllers/patientController');
+const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 // Route to get all patients
 router.route('/')
-    .get(getPatients)   // Get all patients
-    .post(addPatient);  // Add a new patient
+    .get(protect, getPatients)   // Get all patients
+    .post(protect, addPatient);  // Add a new patient
 
 // Route to get a patient by ID
 router.route('/:id')
-    .get(getPatientById); // Get a specific patient by ID
+    .get(protect, getPatientById); // Get a specific patient by ID
 
 module.exports = router;
 
